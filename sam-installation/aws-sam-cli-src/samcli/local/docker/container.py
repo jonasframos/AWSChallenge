874c27358ad4b621fc925d2e54d11de91aac8da1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:21:57 +0000

    Adding numeric_owner as keyword arguement

--- sam-installation/aws-sam-cli-src/samcli/local/docker/container.py
+++ sam-installation/aws-sam-cli-src/samcli/local/docker/container.py
@@ -350,7 +350,7 @@ class Container:
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, path=to_host_path)
